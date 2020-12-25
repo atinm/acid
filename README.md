e2e2de43355ea86cbed302c2bc39f9f08f5b9fb8
Atin M <atinm.dev@gmail.com>
2020-12-25 15:58:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Atin's Chess Information Database
+
+This is an ElectronJS based App for being able to use PGN files.
+
+# Building and Running
+
+    yarn install
+    yarn start
---
+++ src/renderer.js
@@ -0,0 +1,4 @@
+const kokopu = require('./lib/kokopu');
+//const Chessground = require('./lib/chessground').Chessground;
+const config = {};
+const ground = Chessground(document.getElementById("board"), config);
